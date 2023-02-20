835f3f525a2a5133b36f21a4175ec1565a452757
Quentin Doulcet <quentinucme@hotmail.fr>
2023-02-20 13:55:59 -0500

    stupid coach can talk now

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -5,6 +5,11 @@ class QuestionsController < ApplicationController
 
   def answer
     @questionscoach = ["I am going to work", "Silly question, get dressed and go to work!", "I don't care, get dressed and go to work!"]
-    raise
+    # raise
+    if params[:answer] == @questionscoach[0]
+      puts "Ca marche"
+    else
+      puts "Ca marche pas"
+    end
   end
 end
